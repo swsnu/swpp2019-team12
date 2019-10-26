48a82b7ce3807d64ef9803e98588a4457d0280ec
Taeyoung Jang <tyj9327@gmail.com>
2019-10-27 03:11:17 +0900

    [BE/DEV] Signup api 수정

--- server/meetingoverflow/urls.py
+++ server/meetingoverflow/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
     path('workspace/', views.create_workspace, name='create_workspace'),
     path('workspace/<int:id>/', views.workspace, name='workspace'),
 
