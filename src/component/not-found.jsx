0aec1ea6d6116c1f30cc208250fc2bba19715e40
Chaemin <chaaaamni@gmail.com>
2019-10-30 15:36:59 +0900

    [FE/SETTING] 변경된 세팅에 맞게 파일 수정

--- src/component/not-found.jsx
+++ src/component/not-found.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-const NotFound = ({ constext }) => {
-    return (
-        <div>
-            <div>Not Found</div>
-            <button>go to back</button>
-        </div>
-    );
-};
+const NotFound = () => (
+    <div>
+        <div>Not Found</div>
+        <button>go to back</button>
+    </div>
+);
 
 export default NotFound;
