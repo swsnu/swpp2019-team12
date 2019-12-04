8056d67eb4403b604a16e75fcac7199f1141b7e9
Chaemin <chaaaamni@gmail.com>
2019-12-05 01:32:40 +0900

    [FE/DEV] Implement Calender Test Component More

--- src/component/blocks/Calendar.jsx
+++ src/component/blocks/Calendar.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import Timeline from 'react-calendar-timeline';
+import Timeline, {
+    TimelineHeaders,
+    SidebarHeader,
+    DateHeader
+} from 'react-calendar-timeline';
 import 'react-calendar-timeline/lib/Timeline.css';
 import moment from 'moment';
 
 import faker from 'faker';
-// import randomColor from "randomcolor";
+import axios from 'axios';
 
-var keys = {
+const keys = {
     groupIdKey: 'id',
     groupTitleKey: 'title',
     groupRightTitleKey: 'rightTitle',
@@ -19,26 +23,55 @@ var keys = {
     groupLabelKey: 'title'
 };
 
+class CreateModal extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: ''
+        };
+    }
+
+    render() {
+        const { handleAddItems } = this.props;
+        const { title } = this.state;
+        return (
+            <div>
+                <div>title</div>
+                <input
+                    onChange={e => this.setState({ title: e.target.value })}
+                    value={title}
+                />
+                <button onClick={title => handleAddItems(title)}>Create</button>
+            </div>
+        );
+    }
+}
+
 class Calendar extends Component {
     constructor(props) {
         super(props);
 
-        const { groups, items } = generateFakeData();
+        // const { groups, items } = this.generateFakeData();
         const defaultTimeStart = moment()
-            .startOf('day')
+            .startOf('month')
             .toDate();
         const defaultTimeEnd = moment()
-            .startOf('day')
-            .add(1, 'day')
+            .startOf('month')
+            .add(1, 'month')
             .toDate();
 
         this.state = {
-            groups,
-            items,
+            groups: [],
+            items: [],
             defaultTimeStart,
-            defaultTimeEnd
+            defaultTimeEnd,
+            created: false,
+            selected: false
         };
     }
+
+    componentDidMount() {}
 
     handleItemMove = (itemId, dragTime, newGroupOrder) => {
         const { items, groups } = this.state;
@@ -77,13 +110,7 @@ class Calendar extends Component {
         console.log('Resized', itemId, time, edge);
     };
 
-    itemRenderer = ({
-        item,
-        timelineContext,
-        itemContext,
-        getItemProps,
-        getResizeProps
-    }) => {
+    itemRenderer = ({ item, itemContext, getItemProps, getResizeProps }) => {
         const {
             left: leftResizeProps,
             right: rightResizeProps
@@ -104,16 +131,15 @@ class Calendar extends Component {
                         borderStyle: 'solid',
                         borderWidth: 1,
                         borderRadius: 4
-                        // borderLeftWidth: itemContext.selected ? 5 : 5,
-                        // borderRightWidth: itemContext.selected ? 5 : 5
                     },
                     onMouseDown: () => {
-                        console.log('on item click', item);
+                        console.log(itemContext);
+                        this.setState({
+                            selected: true
+                        });
                     }
                 })}>
-                {itemContext.useResizeHandle ? (
-                    <div {...leftResizeProps} />
-                ) : null}
+                <div {...leftResizeProps} />
 
                 <div
                     style={{
@@ -125,21 +151,105 @@ class Calendar extends Component {
                     }}>
                     {itemContext.title}
                 </div>
-
-                {itemContext.useResizeHandle ? (
-                    <div {...rightResizeProps} />
-                ) : null}
+                <div {...rightResizeProps} />
             </div>
         );
     };
 
+    onCanvasClick = (groupId, time, e) => {
+        const { items, created } = this.state;
+
+        const newItem = {
+            flag: 'temp',
+            id: items.length + 1,
+            group: groupId,
+            title: '',
+            start: time,
+            // default two day
+            end: time + 2 * 24 * 3600 * 1000,
+            className:
+                moment(time).day() === 6 || moment(time).day() === 0
+                    ? 'item-weekend'
+                    : '',
+            itemProps: {
+                'data-tip': faker.hacker.phrase()
+            }
+        };
+
+        if (created) {
+            this.setState(
+                {
+                    created: false,
+                    items: this.state.items.filter(item => item.flag !== 'temp')
+                },
+                () =>
+                    this.setState({
+                        created: true,
+                        items: [...this.state.items, newItem]
+                    })
+            );
+        } else {
+            this.setState({
+                created: true,
+                items: [...this.state.items, newItem]
+            });
+        }
+    };
+
+    generateFakeData = (groupCount = 10, itemCount = 100, daysInPast = 30) => {
+        let groups = [];
+        for (let i = 0; i < groupCount; i++) {
+            groups.push({
+                id: `${i + 1}`,
+                title: faker.name.firstName(),
+                rightTitle: faker.name.lastName()
+            });
+        }
+
+        let items = [];
+        for (let i = 0; i < itemCount; i++) {
+            const startDate =
+                faker.date.recent(daysInPast).valueOf() +
+                daysInPast * 0.3 * 86400 * 1000;
+            const startValue =
+                Math.floor(moment(startDate).valueOf() / 10000000) * 10000000;
+            const endValue = moment(
+                startDate +
+                    faker.random.number({ min: 2, max: 10 }) * 12 * 3600 * 1000
+            ).valueOf();
+
+            items.push({
+                id: i + '',
+                group: faker.random.number({ min: 1, max: groups.length }) + '',
+                title: faker.hacker.phrase(),
+                start: startValue,
+                end: endValue,
+                className:
+                    moment(startDate).day() === 6 ||
+                    moment(startDate).day() === 0
+                        ? 'item-weekend'
+                        : '',
+                itemProps: {
+                    'data-tip': faker.hacker.phrase()
+                }
+            });
+        }
+
+        items = items.sort((a, b) => b - a);
+
+        return { groups, items };
+    };
+
     render() {
-        const { groups, items, defaultTimeStart, defaultTimeEnd } = this.state;
+        const {
+            groups,
+            items,
+            defaultTimeStart,
+            defaultTimeEnd,
+            selected
+        } = this.state;
         return (
-            <div
-                className="full-size-block-container Calendar"
-                // onClick={this.props.handleClickBlock}
-            >
+            <div className="full-size-block-container Calendar">
                 <div className="full-size-block-title">
                     <div className="full-size-block-title__text">Calendar</div>
                 </div>
@@ -158,67 +268,20 @@ class Calendar extends Component {
                     itemRenderer={this.itemRenderer}
                     onItemMove={this.handleItemMove}
                     onItemResize={this.handleItemResize}
-                />
-
-                {/* <div className="full-size-block-content">
-                    <div className="full-size-block-content__text">
-                        {this.state.content}
-                    </div>
-                </div> */}
+                    onCanvasClick={this.onCanvasClick}>
+                    <TimelineHeaders className="sticky">
+                        <SidebarHeader>
+                            {({ getRootProps }) => {
+                                return <div {...getRootProps()}>Left</div>;
+                            }}
+                        </SidebarHeader>
+                        <DateHeader unit="primaryHeader" />
+                        <DateHeader />
+                    </TimelineHeaders>
+                </Timeline>
             </div>
         );
     }
 }
 
 export default Calendar;
-
-const generateFakeData = (
-    groupCount = 30,
-    itemCount = 1000,
-    daysInPast = 30
-) => {
-    let randomSeed = Math.floor(Math.random() * 1000);
-    let groups = [];
-    for (let i = 0; i < groupCount; i++) {
-        groups.push({
-            id: `${i + 1}`,
-            title: faker.name.firstName(),
-            rightTitle: faker.name.lastName()
-            // bgColor: randomColor({ luminosity: "light", seed: randomSeed + i })
-        });
-    }
-
-    let items = [];
-    for (let i = 0; i < itemCount; i++) {
-        const startDate =
-            faker.date.recent(daysInPast).valueOf() +
-            daysInPast * 0.3 * 86400 * 1000;
-        const startValue =
-            Math.floor(moment(startDate).valueOf() / 10000000) * 10000000;
-        const endValue = moment(
-            startDate +
-                faker.random.number({ min: 2, max: 20 }) * 15 * 60 * 1000
-        ).valueOf();
-
-        items.push({
-            id: i + '',
-            group: faker.random.number({ min: 1, max: groups.length }) + '',
-            title: faker.hacker.phrase(),
-            start: startValue,
-            end: endValue,
-            // canMove: startValue > new Date().getTime(),
-            // canResize: 'both',
-            className:
-                moment(startDate).day() === 6 || moment(startDate).day() === 0
-                    ? 'item-weekend'
-                    : '',
-            itemProps: {
-                'data-tip': faker.hacker.phrase()
-            }
-        });
-    }
-
-    items = items.sort((a, b) => b - a);
-
-    return { groups, items };
-};
