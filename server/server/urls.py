0e58fad1833ffad8a6bd2bef4a0b6cc8d08257d6
Taeyoung Jang <tyj9327@gmail.com>
2019-12-15 22:46:28 +0900

    [FE]Block components code smell fix

--- server/server/urls.py
+++ server/server/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.static import serve
-# from django.views import views
 
 urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
