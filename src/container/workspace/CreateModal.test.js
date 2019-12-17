a413b1ea56e42650ecdc5a279406170e1f333de0
Chaemin <chaaaamni@gmail.com>
2019-12-17 20:35:02 +0900

    [TEST] CreateModal - workspace

--- src/container/workspace/CreateModal.test.js
+++ src/container/workspace/CreateModal.test.js
@@ -73,6 +73,15 @@ const mockWorkspaces = [
         name: 'test'
     }
 ];
+const mockProfile = {
+    id: 1,
+    nickname: 'test',
+    user: 1
+};
+const mockUser = {
+    id: 1,
+    email: 'test@test.com'
+};
 
 describe('<CreateModal />', () => {
     let component;
@@ -85,22 +94,60 @@ describe('<CreateModal />', () => {
     let mockEvent = { stopPropagation: jest.fn() };
 
     beforeEach(() => {
+        delete window.location;
+        window.location = { reload: jest.fn() };
         axios.get = jest.fn(url => {
-            return new Promise((resolve, reject) => {
-                const result = {
-                    status: 200,
-                    data: {
-                        workspaces: mockWorkspaces,
-                        workspace: mockWorkspace,
-                        admins: mockAdmins,
-                        members: mockMembers,
-                        agendas: mockAgendas,
-                        notes: mockNotes,
-                        todos: mockTodos
-                    }
-                };
-                resolve(result);
-            });
+            if (url === '/api/profile/') {
+                return new Promise((resolve, reject) => {
+                    const result = {
+                        status: 200,
+                        data: {
+                            user: { id: 1, username: 'test' }
+                        }
+                    };
+                    resolve(result);
+                });
+            } else
+                return new Promise((resolve, reject) => {
+                    const result = {
+                        status: 200,
+                        data: {
+                            workspaces: mockWorkspaces,
+                            workspace: mockWorkspace,
+                            admins: mockAdmins,
+                            members: mockMembers,
+                            agendas: mockAgendas,
+                            notes: mockNotes,
+                            todos: mockTodos
+                        }
+                    };
+                    resolve(result);
+                });
+        });
+        axios.post = jest.fn(url => {
+            if (url === '/api/profile/') {
+                return new Promise((resolve, reject) => {
+                    const result = {
+                        status: 200,
+                        data: {
+                            mockUser,
+                            mockProfile
+                        }
+                    };
+                    resolve(result);
+                });
+            } else {
+                return new Promise((resolve, reject) => {
+                    const result = {
+                        status: 201,
+                        data: {
+                            id: mockWorkspace.id,
+                            name: mockWorkspace.name
+                        }
+                    };
+                    resolve(result);
+                });
+            }
         });
 
         workspaceComponent = shallow(<Workspace history={mockHistory} />);
@@ -119,18 +166,6 @@ describe('<CreateModal />', () => {
         workspaceComponent.update();
         workspaceInstance = workspaceComponent.instance();
 
-        axios.get = jest.fn(url => {
-            return new Promise((resolve, reject) => {
-                const result = {
-                    status: 200,
-                    data: {
-                        addedMember: [{ id: 1, username: 'test' }],
-                        addedAdmin: [{ id: 1, username: 'test' }]
-                    }
-                };
-                resolve(result);
-            });
-        });
         component = shallow(
             <CreateModal
                 history={mockHistory}
@@ -144,6 +179,7 @@ describe('<CreateModal />', () => {
     });
     afterEach(() => {
         jest.clearAllMocks();
+        window.location = location;
     });
 
     it('should render without error', () => {
@@ -157,12 +193,178 @@ describe('<CreateModal />', () => {
         expect(mockEvent.stopPropagation).toHaveBeenCalledTimes(1);
     });
 
-    /*
-    it('render label', () => {
-        component = shallow(<CreateModal history={mockHistory} />);
+    it('after component did mount', async () => {
+        await instance.componentDidMount();
 
-        let labelWrapper = component.find('.createModal-label');
-        expect(labelWrapper.length).toBe(1);
+        const expected = {
+            addedMember: [{ id: 1, username: 'test' }],
+            addedMemberId: [1],
+            addedAdmin: [{ id: 1, username: 'test' }],
+            addedAdminId: [1],
+
+            title: '',
+            emailMember: '',
+            emailAdmin: '',
+            searchedMember: [],
+            searchedAdmin: []
+        };
+        expect(instance.state).toEqual(expected);
     });
-    */
+
+    it('render label', () => {
+        let wrapper = component.find('.createModal-label');
+        expect(wrapper.length).toBe(1);
+    });
+
+    it('render title', () => {
+        let wrapper = component.find('.createModal-title');
+        expect(wrapper.length).toBe(1);
+
+        wrapper = component.find('.createModal-title__input');
+        wrapper.simulate('change', { target: { value: 'test' } });
+
+        expect(instance.state.title).toEqual('test');
+    });
+    it('render modal member', async () => {
+        let wrapper = component.find('.createModal-member');
+        expect(wrapper.length).toBe(2);
+
+        wrapper = component.find('.createModal-member__input').at(0);
+        await wrapper.simulate('change', { target: { value: 'test' } });
+        expect(instance.state.emailMember).toEqual('test');
+        expect(instance.state.searchedMember).toEqual({
+            mockUser,
+            mockProfile
+        });
+
+        // handleSearch
+
+        await wrapper.simulate('change', { target: { value: '' } });
+        expect(instance.state.emailMember).toEqual('');
+        expect(instance.state.searchedMember).toEqual([]);
+
+        // handle Search
+
+        component.setState({
+            searchedMember: [{ profile: mockProfile, user: mockUser }]
+        });
+        component.update();
+
+        expect(wrapper.length).toBe(1);
+        wrapper = component.find('.createModal-member__member--searched-email');
+        expect(wrapper.length).toBe(1);
+
+        wrapper.simulate('click');
+        const { addedMember, addedMemberId } = instance.state;
+        expect(instance.state).toEqual({
+            ...instance.state,
+            addedMember,
+            addedMemberId,
+            emailMember: '',
+            searchedMember: []
+        });
+    });
+    it('render modal admin', async () => {
+        let wrapper = component.find('.createModal-member');
+        expect(wrapper.length).toBe(2);
+
+        wrapper = component.find('.createModal-member__input').at(1);
+        await wrapper.simulate('change', { target: { value: 'test' } });
+        expect(instance.state.emailAdmin).toEqual('test');
+        expect(instance.state.searchedAdmin).toEqual({ mockUser, mockProfile });
+
+        // handle search
+        await wrapper.simulate('change', { target: { value: '' } });
+        expect(instance.state.emailAdmin).toEqual('');
+        expect(instance.state.searchedAdmin).toEqual([]);
+
+        component.setState({
+            searchedAdmin: [{ profile: mockProfile, user: mockUser }]
+        });
+        component.update();
+
+        expect(wrapper.length).toBe(1);
+        wrapper = component.find('.createModal-member__member--searched-email');
+        expect(wrapper.length).toBe(1);
+
+        wrapper.simulate('click');
+        const { addedAdmin, addedAdminId } = instance.state;
+        expect(instance.state).toEqual({
+            ...instance.state,
+            addedAdmin,
+            addedAdminId,
+            emailAdmin: '',
+            searchedAdmin: []
+        });
+    });
+
+    it('render delete member', () => {
+        let wrapper = component.find('.createModal-member__member--added');
+        expect(wrapper.length).toBe(2);
+
+        component.setState({
+            addedMember: [{ profile: mockProfile, user: mockUser }]
+        });
+        component.update();
+
+        wrapper = component.find('.createModal-member__member--added-element');
+        expect(wrapper.length).toBe(1);
+
+        wrapper.simulate('click');
+        expect(instance.state).toEqual({
+            ...instance.state,
+            addedMember: []
+        });
+    });
+
+    it('render delete Admin', () => {
+        let wrapper = component.find('.createModal-member__member--added');
+        expect(wrapper.length).toBe(2);
+
+        component.setState({
+            addedAdmin: [{ profile: mockProfile, user: mockUser }]
+        });
+        component.update();
+
+        wrapper = component.find('.createModal-member__member--added-element');
+        expect(wrapper.length).toBe(1);
+
+        wrapper.simulate('click');
+        expect(instance.state).toEqual({
+            ...instance.state,
+            addedAdmin: []
+        });
+    });
+
+    it('create validation', async () => {
+        const invalid = instance.handleCreateValidation();
+        expect(invalid).toEqual(false);
+
+        let wrapper = component.find('.createModal-button-container .disabled');
+        expect(wrapper.length).toEqual(1);
+
+        component.setState({
+            title: 'test',
+            addedMember: [mockUser]
+        });
+        component.update();
+        const valid = instance.handleCreateValidation();
+        expect(valid).toEqual(true);
+
+        wrapper = component.find('.createModal-button-container .primary');
+        expect(wrapper.length).toEqual(1);
+
+        await wrapper.simulate('click');
+        expect(mockHistory.push).toHaveBeenCalledTimes(1);
+        expect(window.location.reload).toHaveBeenCalledTimes(1);
+    });
+    it('cancel', async () => {
+        let wrapper = component.find(
+            '.createModal-button-container .modal-cancel'
+        );
+        expect(wrapper.length).toEqual(1);
+
+        await wrapper.simulate('click');
+        expect(workspaceInstance.state.showCreateWorkspaceModal).toEqual(false);
+    });
 });
