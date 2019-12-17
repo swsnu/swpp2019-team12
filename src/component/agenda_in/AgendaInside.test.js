0b264ffef75ac219e1c919dfc35b74c182310438
Taeyoung Jang <tyj9327@gmail.com>
2019-12-18 01:31:50 +0900

    [TEST]AgendaInside modified

--- src/component/agenda_in/AgendaInside.test.js
+++ src/component/agenda_in/AgendaInside.test.js
@@ -35,8 +35,43 @@ describe('<AgendaInside />', () => {
         expect(wrapper.length).toBe(1);
     });
 
-    // it('should render getderivedstate properly', () => {
-    //     const component = shallow(agendaInside);
-    //     let instance = component.instance();
-    // });
+    it('should render getderivedstate properly', () => {
+        const component = shallow(agendaInside);
+        let instance = component.instance();
+        instance.setState({
+            blocks: stubBlocks
+        });
+        const stubPrev = {
+            noteId: 1,
+            agendaId: 1,
+            blocks: [{ id: 1 }, { id: 2 }]
+        };
+        const stubNext = {
+            noteId: 1,
+            agendaId: 1,
+            blocks: [
+                {
+                    block_type: 'Text',
+                    id: 1,
+                    content: 'content',
+                    document_id: 'docId'
+                }
+            ]
+        };
+        let result = instance.constructor.getDerivedStateFromProps(
+            stubPrev,
+            stubNext
+        );
+        expect(result).toEqual({
+            blocks: [
+                {
+                    content: <div>not implemented yet</div>,
+                    id: 'undefined-0-1'
+                },
+                { content: <div>not implemented yet</div>, id: 'undefined-1-2' }
+            ]
+        });
+        result = instance.constructor.getDerivedStateFromProps([], []);
+        expect(result).toEqual(null);
+    });
 });
